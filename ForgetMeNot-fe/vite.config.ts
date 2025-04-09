07cc7f04504fda1f904ea6d8073a3da5d2124526
Antonia Amosova <amosova1@uniba.sk>
2025-04-09 17:19:15 +0200

    Prepare for https

--- ForgetMeNot-fe/vite.config.ts
+++ ForgetMeNot-fe/vite.config.ts
@@ -2,9 +2,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import fs from 'fs';
 
 export default defineConfig({
   server: {
+    // https: {
+    //   key: fs.readFileSync('./path/to/your/private.key'), // Path to your private key
+    //   cert: fs.readFileSync('./path/to/your/certificate.crt'), // Path to your certificate
+    // },
     port: 5173,
     proxy: {
       "/api": {
